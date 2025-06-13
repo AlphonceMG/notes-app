518c314a70994e01adf43e93108063787c09dcb5
Alphonce Muthondu <alphoncegikonyo10@gmail.com>
2025-06-13 12:04:33 +0300

    Initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer(){
+
+     const year =  new Date().getFullYear()
+
+    return(
+        <footer>
+            <p>
+                Copyright  {`${year}`}
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+
+function Header(){
+<header>
+    <h1>
+        Keeper App
+    </h1>
+</header>
+}
+export default Header;
